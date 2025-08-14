6a314b4b6ea19611a0ca453c3d7600572a6462aa
tivui <tivui64@gmail.com>
2025-08-14 20:15:33 +0200

    update angular 20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { TodosComponent } from './todos/todos.component';
-import { Amplify } from 'aws-amplify';
-import outputs from '../../amplify_outputs.json';
-
-Amplify.configure(outputs);
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, TodosComponent]
+    imports: [TodosComponent]
 })
 export class AppComponent {
   title = 'amplify-angular-template';
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -12,7 +12,7 @@ const client = generateClient<Schema>();
     styleUrl: './todos.component.css'
 })
 export class TodosComponent implements OnInit {
-  todos: any[] = [];
+  todos: unknown[] = [];
 
   ngOnInit(): void {
     this.listTodos();
@@ -21,7 +21,7 @@ export class TodosComponent implements OnInit {
   listTodos() {
     try {
       client.models.Todo.observeQuery().subscribe({
-        next: ({ items, isSynced }) => {
+        next: ({ items }) => {
           this.todos = items;
         },
       });
