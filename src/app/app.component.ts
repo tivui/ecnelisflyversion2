4bf5291f9d24a102e26e58e811e2b7406f973396
tivui <tivui64@gmail.com>
2025-08-14 22:42:15 +0200

    maj ng 18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { TodosComponent } from './todos/todos.component';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [TodosComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, TodosComponent],
 })
 export class AppComponent {
   title = 'amplify-angular-template';
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -6,10 +6,11 @@ import type { Schema } from '../../../amplify/data/resource';
 const client = generateClient<Schema>();
 
 @Component({
-    selector: 'app-todos',
-    imports: [CommonModule],
-    templateUrl: './todos.component.html',
-    styleUrl: './todos.component.css'
+  selector: 'app-todos',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './todos.component.html',
+  styleUrl: './todos.component.css',
 })
 export class TodosComponent implements OnInit {
   todos: unknown[] = [];
