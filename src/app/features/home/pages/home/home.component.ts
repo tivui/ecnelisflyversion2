bd354e7900b87e8bd42a482957bc1201630e1c0e
tivui <tivui64@gmail.com>
2025-08-22 15:20:11 +0200

    cats signal to observable

--- src/app/features/home/pages/home/home.component.ts
+++ src/app/features/home/pages/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppUserService } from '../../../../core/services/app-user.service';
 import { AppUser } from '../../../../core/models/app-user.model';
-import { Observable } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-home',
@@ -15,5 +15,6 @@ export class HomeComponent {
   private readonly appUserService = inject(AppUserService);
 
   // Observable of the current AppUser
-  public appUser$: Observable<AppUser | null> = this.appUserService.currentUser$;
+    public appUser = toSignal<AppUser | null>(this.appUserService.currentUser$, { initialValue: null });
+
 }
