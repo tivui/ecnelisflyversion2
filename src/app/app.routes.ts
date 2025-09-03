569295f1c758b7b2b2a9e16117e9068a0299db76
tivui <tivui64@gmail.com>
2025-09-03 20:39:25 +0200

    nav avec router link pour database

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,26 @@
+// app.routes.ts
 import { Routes } from '@angular/router';
 import { authGuard } from './core/services/auth-guard.service';
 import { HomeComponent } from './features/home/pages/home/home.component';
 import { LoginComponent } from './core/pages/auth/login/login.component';
 import { AccountComponent } from './features/users/pages/account/account/account.component';
-import { ImportSoundsComponent } from './features/admin/database/import-sounds/import-sounds.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
   { path: 'account', component: AccountComponent, canActivate: [authGuard] },
-  { path: 'admin/database/import-sounds', component: ImportSoundsComponent, canActivate: [authGuard], data: { requiredGroup: 'ADMIN' } },
+
+  // Nouvelle section database avec lazy loading
+  {
+    path: 'admin/database',
+    loadChildren: () =>
+      import('./features/admin/database/database.routes').then(
+        (m) => m.DATABASE_ROUTES,
+      ),
+    canActivate: [authGuard],
+    data: { requiredGroup: 'ADMIN' },
+  },
+
   { path: 'login', component: LoginComponent },
-  { path: '**', redirectTo: 'home' }
+  { path: '**', redirectTo: 'home' },
 ];
