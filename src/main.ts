fe864c1d537191ad41e25b16ecc55086ade123e5
tivui <tivui64@gmail.com>
2025-10-25 14:18:42 +0200

    nouveau test affichage mobile

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { appConfig } from './app/app.config'
+import { appConfig } from './app/app.config';
 import { Amplify } from 'aws-amplify';
 import outputs from '../amplify_outputs.json';
 
 // Configure Amplify avec la sortie backend générée
 Amplify.configure(outputs);
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err),
+);
+
+// 🔹 Cette partie JS permet de corriger le problème des barres de navigation mobiles
+// qui recouvrent parfois le contenu. On calcule la vraie hauteur visible du viewport.
+function setRealVh() {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--real-vh', `${vh}px`);
+}
+setRealVh();
+window.addEventListener('resize', setRealVh);
+window.addEventListener('orientationchange', setRealVh);
