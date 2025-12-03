d0ca92fcc94de9967501fd77bb60bdfda6fa6e81
tivui <tivui64@gmail.com>
2025-12-03 20:04:34 +0100

    lien vers les cartes par catégories

--- src/app/features/home/pages/home/widgets/card-category/card-category.component.ts
+++ src/app/features/home/pages/home/widgets/card-category/card-category.component.ts
@@ -1,6 +1,7 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, input } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-card-category',
@@ -8,9 +9,20 @@ import { MatIconModule } from '@angular/material/icon';
   imports: [MatCardModule, MatIconModule],
   templateUrl: './card-category.component.html',
   styleUrl: './card-category.component.scss',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class CardCategoryComponent {
-  title = input<string>('');
+  title = input.required<string>();
+  category = input.required<string>();
   icon = input<string>('');
+
+  private readonly router = inject(Router);
+
+  goToMapflyCategory() {
+    this.router.navigate(['/mapfly'], {
+      queryParams: {
+        category: this.category(),
+      },
+    });
+  }
 }
