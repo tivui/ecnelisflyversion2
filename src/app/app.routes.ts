0e083beae8ee19b94733f82bb87820d6d744e64a
tivui <tivui64@gmail.com>
2025-12-30 18:44:02 +0100

    structure stepper

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,13 @@ import { HomeComponent } from './features/home/pages/home/home.component';
 import { LoginComponent } from './core/pages/auth/login/login.component';
 import { AccountComponent } from './features/users/pages/account/account/account.component';
 import { MapflyComponent } from './features/map/pages/mapfly/mapfly.component';
+import { NewSoundComponent } from './features/new-sound/pages/new-sound/new-sound.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
   { path: 'account', component: AccountComponent, canActivate: [authGuard] },
-
+  { path: 'new-sound', component: NewSoundComponent, canActivate: [authGuard] },
   // Nouvelle section database avec lazy loading
   {
     path: 'admin/database',
---
+++ src/app/features/new-sound/pages/new-sound/new-sound.component.ts
@@ -0,0 +1,56 @@
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import {
+  StepperOrientation,
+  MatStepperModule,
+} from '@angular/material/stepper';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { AsyncPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-new-sound',
+  standalone: true,
+  imports: [
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    AsyncPipe,
+  ],
+  templateUrl: './new-sound.component.html',
+  styleUrl: './new-sound.component.scss',
+})
+export class NewSoundComponent {
+  private _formBuilder = inject(FormBuilder);
+
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  thirdFormGroup = this._formBuilder.group({
+    thirdCtrl: ['', Validators.required],
+  });
+  stepperOrientation: Observable<StepperOrientation>;
+
+  constructor() {
+    const breakpointObserver = inject(BreakpointObserver);
+
+    this.stepperOrientation = breakpointObserver
+      .observe('(min-width: 800px)')
+      .pipe(map(({ matches }) => (matches ? 'horizontal' : 'vertical')));
+  }
+}
